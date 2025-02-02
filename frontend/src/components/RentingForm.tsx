c91f673d0177158b345bf4044fb3db1083a7d57d
Ousmane Barry <kegnekobarry822@gmail.com>
2025-02-02 16:30:12 -0500

    first commit

--- frontend/src/components/RentingForm.tsx
+++ frontend/src/components/RentingForm.tsx
@@ -1,125 +1,91 @@
-import { useEffect, useCallback } from "react";
-import { useDisclosure } from "@mantine/hooks";
-import { useForm, isNotEmpty, isInRange } from "@mantine/form";
-import {
-  Button,
-  Stack,
-  Group,
-  NumberInput,
-  Text,
-  Center,
-  Box,
-} from "@mantine/core";
-import DateRangePicker from "~/components/DateRangePicker";
-import AddPaymentInfo from "./AddPaymentInfo";
-import { RentingFormInfo } from "~/types";
+import { useEffect, useCallback } from 'react';
+import { useDisclosure } from '@mantine/hooks';
+import { useForm, isNotEmpty, isInRange } from '@mantine/form';
+import { Button, Stack, Group, NumberInput, Text, Center } from '@mantine/core';
+import DateRangePicker from '~/components/DateRangePicker';
+import AddPaymentInfo from './AddPaymentInfo';
+import { RentingFormInfo } from '~/types';
 
 interface HotelForm {
-  setFormReset?: (formReset: () => void) => void;
-  onSubmit: (hotel: RentingFormInfo) => void;
+	setFormReset?: (formReset: () => void) => void;
+	onSubmit: (hotel: RentingFormInfo) => void;
 }
 
 export default function RentingForm(props: HotelForm) {
-  const [opened, { open, close }] = useDisclosure();
+	const [opened, { open, close }] = useDisclosure();
 
-  const form = useForm({
-    initialValues: {
-      discount: 0,
-      additional_charges: 0,
-      hotel_ID: null,
-      room_number: null,
-      customer_SSN_SIN: null,
-      check_in_date: "",
-      check_out_date: "",
-    },
-    validate: {
-      discount: isInRange(
-        { min: 0, max: 100 },
-        "Discount must be between 0 to 100"
-      ),
-      additional_charges: isInRange(
-        { min: 0 },
-        "Additional charges must be positive"
-      ),
-      hotel_ID: isNotEmpty("Provide a hotel id"),
-      room_number: isNotEmpty("Provide a room number"),
-      customer_SSN_SIN: isNotEmpty("Provide customer SSN"),
-      check_in_date: isNotEmpty("Select check in and check out dates"),
-      check_out_date: isNotEmpty("Select check in and check out dates"),
-    },
-  });
+	const form = useForm({
+		initialValues: {
+			discount: 0,
+			additional_charges: 0,
+			hotel_ID: null,
+			room_number: null,
+			customer_SSN_SIN: null,
+			check_in_date: '',
+			check_out_date: '',
+		},
+		validate: {
+			discount: isInRange({ min: 0, max: 100 }, 'Discount must be between 0 to 100'),
+			additional_charges: isInRange({ min: 0 }, 'Additional charges must be positive'),
+			hotel_ID: isNotEmpty('Provide a hotel id'),
+			room_number: isNotEmpty('Provide a room number'),
+			customer_SSN_SIN: isNotEmpty('Provide customer SSN'),
+			check_in_date: isNotEmpty('Select check in and check out dates'),
+			check_out_date: isNotEmpty('Select check in and check out dates'),
+		},
+	});
 
-  const handleDateChange = useCallback(
-    (dateRange: string[]) => {
-      form.setValues({
-        check_in_date: dateRange[0],
-        check_out_date: dateRange[1],
-      });
-    },
-    [form.setValues]
-  );
+	const handleDateChange = useCallback(
+		(dateRange: string[]) => {
+			form.setValues({
+				check_in_date: dateRange[0],
+				check_out_date: dateRange[1],
+			});
+		},
+		[form.setValues]
+	);
 
-  useEffect(() => {
-    if (props.setFormReset) props.setFormReset(() => form.reset);
-  }, [form.reset, props.setFormReset]);
+	useEffect(() => {
+		if (props.setFormReset) props.setFormReset(() => form.reset);
+	}, [form.reset, props.setFormReset]);
 
-  return (
-    <Stack spacing="md">
-      <NumberInput
-        placeholder="Customer SSN"
-        label="Customer SSN"
-        {...form.getInputProps("customer_SSN_SIN")}
-      />
-      <Group position="apart">
-        <NumberInput
-          placeholder="Hotel Id"
-          label="Hotel Id"
-          {...form.getInputProps("hotel_ID")}
-        />
-        <NumberInput
-          placeholder="Room number"
-          label="Room number"
-          {...form.getInputProps("room_number")}
-        />
-      </Group>
-      <Group position="apart">
-        <NumberInput
-          placeholder="Discount"
-          icon="%"
-          label="Discount"
-          {...form.getInputProps("discount")}
-          sx={{ maxWidth: "225px" }}
-        />
-        <NumberInput
-          placeholder="Additional Charges"
-          icon="$"
-          label="Additional Charges"
-          {...form.getInputProps("additional_charges")}
-          sx={{ maxWidth: "225px" }}
-        />
-      </Group>
-      <Center>
-        <Stack sx={{ padding: "10px 0px" }}>
-          <DateRangePicker
-            setDateRange={handleDateChange}
-            startDateLabel="Check In"
-            endDateLabel="Check Out"
-          />
-          <Text color="red" size="xs" sx={{ marginTop: "-10px" }}>
-            {form.errors.check_in_date}
-          </Text>
-        </Stack>
-      </Center>
-      <Group position="apart">
-        <AddPaymentInfo complete={opened} setComplete={open} />
-        <Button
-          type="submit"
-          onClick={form.onSubmit(props.onSubmit as any) as any}
-          disabled={!opened}
-        >
-          Submit
-        </Button>
-      </Group>
-    </Stack>
-  );
+	return (
+		<Stack spacing='md'>
+			<NumberInput placeholder='Customer SSN' label='Customer SSN' {...form.getInputProps('customer_SSN_SIN')} />
+			<Group position='apart'>
+				<NumberInput placeholder='Hotel Id' label='Hotel Id' {...form.getInputProps('hotel_ID')} />
+				<NumberInput placeholder='Room number' label='Room number' {...form.getInputProps('room_number')} />
+			</Group>
+			<Group position='apart'>
+				<NumberInput
+					placeholder='Discount'
+					icon='%'
+					label='Discount'
+					{...form.getInputProps('discount')}
+					sx={{ maxWidth: '225px' }}
+				/>
+				<NumberInput
+					placeholder='Additional Charges'
+					icon='$'
+					label='Additional Charges'
+					{...form.getInputProps('additional_charges')}
+					sx={{ maxWidth: '225px' }}
+				/>
+			</Group>
+			<Center>
+				<Stack sx={{ padding: '10px 0px' }}>
+					<DateRangePicker setDateRange={handleDateChange} startDateLabel='Check In' endDateLabel='Check Out' />
+					<Text color='red' size='xs' sx={{ marginTop: '-10px' }}>
+						{form.errors.check_in_date}
+					</Text>
+				</Stack>
+			</Center>
+			<Group position='apart'>
+				<AddPaymentInfo complete={opened} setComplete={open} />
+				<Button type='submit' onClick={form.onSubmit(props.onSubmit as any) as any} disabled={!opened}>
+					Submit
+				</Button>
+			</Group>
+		</Stack>
+	);
 }
--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,205 +1,171 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-import Cookies from "js-cookie";
-import { useDisclosure } from "@mantine/hooks";
-import {
-  Paper,
-  Text,
-  Group,
-  Rating,
-  Modal,
-  Stack,
-  Button,
-  Box,
-} from "@mantine/core";
-import { message } from "antd";
-import RoomForm from "~/components/RoomForm";
-import useSearchQuery from "~/utils/useSearchQuery";
-import useToken from "~/utils/useToken";
-import { RoomInfo, HotelInfo, HotelChainInfo } from "~/types";
+import axios from 'axios';
+import { useState } from 'react';
+import Cookies from 'js-cookie';
+import { useDisclosure } from '@mantine/hooks';
+import { Paper, Text, Group, Rating, Modal, Stack, Button, Box } from '@mantine/core';
+import { message } from 'antd';
+import RoomForm from '~/components/RoomForm';
+import useSearchQuery from '~/utils/useSearchQuery';
+import useToken from '~/utils/useToken';
+import { RoomInfo, HotelInfo, HotelChainInfo } from '~/types';
 
 interface RoomProps {
-  room: RoomInfo;
-  hotel: HotelInfo;
-  hotelChains: HotelChainInfo[];
+	room: RoomInfo;
+	hotel: HotelInfo;
+	hotelChains: HotelChainInfo[];
 }
 
 export default function Room(props: RoomProps) {
-  const [opened, { open, close }] = useDisclosure();
-  //   const [hotel, setHotel] = useState<HotelInfo | null>();
-  const [room, setRoom] = useState(props.room);
-  const [display, setDisplay] = useState(true);
-  const [startDate, endDate] = useSearchQuery((state) => [
-    state.startDate,
-    state.endDate,
-  ]);
+	const [opened, { open, close }] = useDisclosure();
+	const [room, setRoom] = useState(props.room);
+	const [display, setDisplay] = useState(true);
+	const [startDate, endDate] = useSearchQuery((state) => [state.startDate, state.endDate]);
 
-  const token = useToken();
+	const token = useToken();
 
-  //   useEffect(() => {
-  //     axios
-  //       .get<HotelInfo>(
-  //         `http://127.0.0.1:5000/hotel/hotel/${props.room.hotel_id}`
-  //       )
-  //       .then((res) => {
-  //         const { data } = res;
-  //         setHotel(data);
-  //       });
-  //   }, [props.room.hotel_id]);
+	const handleDelete = async () => {
+		try {
+			const access_token = Cookies.get('access_token');
 
-  const handleDelete = async () => {
-    try {
-      const access_token = Cookies.get("access_token");
+			await axios.delete(`${process.env.NEXT_PUBLIC_URL}/room/room/${props.hotel.hotel_ID}/${props.room.room_number}`, {
+				headers: { Authorization: `Bearer ${access_token}` },
+			});
 
-      await axios.delete(
-        `${process.env.NEXT_PUBLIC_URL}/room/room/${props.hotel.hotel_ID}/${props.room.room_number}`,
-        { headers: { Authorization: `Bearer ${access_token}` } }
-      );
+			setDisplay(false);
+			message.success('Room successfully deleted');
+		} catch (error: any) {
+			message.error(error.response.data.message);
+		}
+	};
 
-      setDisplay(false);
-      message.success("Room successfully deleted");
-    } catch (error: any) {
-      message.error(error.response.data.message);
-    }
-  };
+	const handleBooking = async () => {
+		try {
+			const access_token = Cookies.get('access_token');
 
-  const handleBooking = async () => {
-    try {
-      const access_token = Cookies.get("access_token");
+			await axios.post(
+				`${process.env.NEXT_PUBLIC_URL}/booking/booking`,
+				{
+					hotel_ID: props.hotel.hotel_ID,
+					room_number: props.room.room_number,
+					scheduled_check_in_date: startDate,
+					scheduled_check_out_date: endDate,
+				},
+				{ headers: { Authorization: `Bearer ${access_token}` } }
+			);
 
-      await axios.post(
-        `${process.env.NEXT_PUBLIC_URL}/booking/booking`,
-        {
-          hotel_ID: props.hotel.hotel_ID,
-          room_number: props.room.room_number,
-          scheduled_check_in_date: startDate,
-          scheduled_check_out_date: endDate,
-        },
-        { headers: { Authorization: `Bearer ${access_token}` } }
-      );
+			setDisplay(false);
+			message.success('Room successfully booked');
+		} catch (error: any) {
+			message.error(error.response.data.message);
+		}
+	};
 
-      setDisplay(false);
-      message.success("Room successfully booked");
-    } catch (error: any) {
-      message.error(error.response.data.message);
-    }
-  };
+	const handleUpdate = async (values: RoomInfo) => {
+		try {
+			const access_token = Cookies.get('access_token');
 
-  const handleUpdate = async (values: RoomInfo) => {
-    try {
-      const access_token = Cookies.get("access_token");
+			await axios.put(
+				`${process.env.NEXT_PUBLIC_URL}/room/room`,
+				{
+					room_number: values.room_number,
+					hotel_ID: values.hotel_id,
+					room_capacity: values.room_capacity,
+					view_type: values.view_type,
+					price_per_night: values.price_per_night,
+					is_extendable: values.is_extendable,
+					room_problems: values.room_problems,
+				},
+				{ headers: { Authorization: `Bearer ${access_token}` } }
+			);
 
-      await axios.put(
-        `${process.env.NEXT_PUBLIC_URL}/room/room`,
-        {
-          room_number: values.room_number,
-          hotel_ID: values.hotel_id,
-          room_capacity: values.room_capacity,
-          view_type: values.view_type,
-          price_per_night: values.price_per_night,
-          is_extendable: values.is_extendable,
-          room_problems: values.room_problems,
-        },
-        { headers: { Authorization: `Bearer ${access_token}` } }
-      );
+			close();
+			setRoom({ ...room, ...values });
+		} catch (error: any) {
+			message.error(error.response.data.message);
+		}
+	};
 
-      close();
-      setRoom({ ...room, ...values });
-    } catch (error: any) {
-      message.error(error.response.data.message);
-    }
-  };
+	if (!display) return <></>;
 
-  if (!display) return <></>;
+	return (
+		<Paper shadow='xs' p='lg' sx={{ width: '400px' }}>
+			<Stack spacing='sm'>
+				<Group position='apart'>
+					<Text size='lg' weight={500}>
+						<Text fw='bold'>Room Number:</Text>
+						{room.room_number}
+					</Text>
+					<Text align='right'>
+						<Text fw='bold'>Hotel Id:</Text>
+						{props.hotel.hotel_ID}
+					</Text>
+				</Group>
+				<Group position='apart'>
+					<Text>
+						<Text fw='bold'>Hotel Chain:</Text>
+						{props.hotelChains.find((hotelChain) => hotelChain.chain_ID === props.hotel.chain_id)?.name}
+					</Text>
 
-  return (
-    <Paper shadow="xs" p="lg" sx={{ width: "400px" }}>
-      <Stack spacing="sm">
-        <Group position="apart">
-          <Text size="lg" weight={500}>
-            <Text fw="bold">Room Number:</Text>
-            {room.room_number}
-          </Text>
-          <Text align="right">
-            <Text fw="bold">Hotel Id:</Text>
-            {props.hotel.hotel_ID}
-          </Text>
-        </Group>
-        <Group position="apart">
-          <Text>
-            <Text fw="bold">Hotel Chain:</Text>
-            {
-              props.hotelChains.find(
-                (hotelChain) => hotelChain.chain_ID === props.hotel.chain_id
-              )?.name
-            }
-          </Text>
+					<Text align='right'>
+						<Text fw='bold'>Star Rating:</Text>
+						<Rating value={props.hotel.star_rating} readOnly />
+					</Text>
+				</Group>
+				<Group position='apart' mt='sm'>
+					<Text>
+						<Text fw='bold'>Address:</Text>
+						{`${props.hotel.address_street_number} ${props.hotel.address_street_name}, ${props.hotel.address_city}`}
+					</Text>
+					<Text align='right'>
+						<Text fw='bold'>Location:</Text>
+						{`${props.hotel.address_province_state}, ${props.hotel.address_country}`}
+					</Text>
+				</Group>
 
-          <Text align="right">
-            <Text fw="bold">Star Rating:</Text>
-            <Rating value={props.hotel.star_rating} readOnly />
-          </Text>
-        </Group>
-        <Group position="apart" mt="sm">
-          <Text>
-            <Text fw="bold">Address:</Text>
-            {`${props.hotel.address_street_number} ${props.hotel.address_street_name}, ${props.hotel.address_city}`}
-          </Text>
-          <Text align="right">
-            <Text fw="bold">Location:</Text>
-            {`${props.hotel.address_province_state}, ${props.hotel.address_country}`}
-          </Text>
-        </Group>
+				<Group position='apart' mt='sm'>
+					<Text>
+						<Text fw='bold'>Room Capacity:</Text>
+						{room.room_capacity}
+					</Text>
 
-        <Group position="apart" mt="sm">
-          <Text>
-            <Text fw="bold">Room Capacity:</Text>
-            {room.room_capacity}
-          </Text>
+					<Text align='right'>
+						<Text fw='bold'>Price Per Night:</Text>${room.price_per_night}
+					</Text>
+				</Group>
 
-          <Text align="right">
-            <Text fw="bold">Price Per Night:</Text>${room.price_per_night}
-          </Text>
-        </Group>
+				<Group position='apart' mt='sm'>
+					<Text>
+						<Text fw='bold'>View Type:</Text>
+						{room.view_type}
+					</Text>
 
-        <Group position="apart" mt="sm">
-          <Text>
-            <Text fw="bold">View Type:</Text>
-            {room.view_type}
-          </Text>
+					<Text align='right'>
+						<Text fw='bold'>Is Extendable:</Text>
+						{room.is_extendable ? 'Yes' : 'No'}
+					</Text>
+				</Group>
+				{token.role === 'customer' && (
+					<Button size='md' compact onClick={handleBooking} disabled={!startDate && !endDate}>
+						Book Now
+					</Button>
+				)}
+				{token.role === 'employee' && (
+					<Group position='apart'>
+						<Button size='md' compact onClick={handleDelete}>
+							Delete
+						</Button>
+						<Button size='md' compact onClick={open}>
+							Edit
+						</Button>
+					</Group>
+				)}
+			</Stack>
 
-          <Text align="right">
-            <Text fw="bold">Is Extendable:</Text>
-            {room.is_extendable ? "Yes" : "No"}
-          </Text>
-        </Group>
-        {token.role === "customer" && (
-          <Button
-            size="md"
-            compact
-            onClick={handleBooking}
-            disabled={!startDate && !endDate}
-          >
-            Book Now
-          </Button>
-        )}
-        {token.role === "employee" && (
-          <Group position="apart">
-            <Button size="md" compact onClick={handleDelete}>
-              Delete
-            </Button>
-            <Button size="md" compact onClick={open}>
-              Edit
-            </Button>
-          </Group>
-        )}
-      </Stack>
-
-      <Modal opened={opened} onClose={close} title="Edit Room">
-        <Box sx={{ padding: "40px" }}>
-          <RoomForm room={props.room} onSubmit={handleUpdate} />
-        </Box>
-      </Modal>
-    </Paper>
-  );
+			<Modal opened={opened} onClose={close} title='Edit Room'>
+				<Box sx={{ padding: '40px' }}>
+					<RoomForm room={props.room} onSubmit={handleUpdate} />
+				</Box>
+			</Modal>
+		</Paper>
+	);
 }
